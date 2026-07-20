41a27626616da1bb190026355e42440c094baa1a
Dakshit Mohan <dakshitmohan@gmail.com>
2026-07-20 22:18:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> isConnected, vector<bool> &visited, int i) {
-        visited[i]= true;
+    vector<int> parent, rank;
 
-        for (int neighbour=0; neighbour<isConnected[i].size(); neighbour++) {
-            if (!visited[neighbour] && isConnected[i][neighbour]==1) {
-                dfs(isConnected, visited, neighbour);
-            }
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py) return;
+
+        if (rank[px] < rank[py])
+            parent[px] = py;
+        else if (rank[px] > rank[py])
+            parent[py] = px;
+        else {
+            parent[py] = px;
+            rank[px]++;
         }
     }
-    
-    int findCircleNum(vector<vector<int>>& isConnected) {
-        int v= isConnected.size();
-        vector<bool> visited(v, false);
-        int count=0;
 
-        int node= 0;
-        for (int i=0; i<v; i++) {
-            if (!visited[i]){
-                dfs(isConnected, visited, i);
-                count++;
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+
+        parent.resize(n);
+        rank.assign(n, 0);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+
+        int provinces = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (isConnected[i][j] && find(i) != find(j)) {
+                    unite(i, j);
+                    provinces--;
+                }
             }
         }
 
-        return count;
+        return provinces;
     }
 };
