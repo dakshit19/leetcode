1ff6cbee514315fa3a69215717d4e25c17ce1304
Dakshit Mohan <dakshitmohan@gmail.com>
2026-07-31 17:56:12 +0530

    Time: 3 ms (50.48%), Space: 23.7 MB (46.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cand1=0, cand2=0;
-        int count1=0, count2=0;
         int n= nums.size();
-
+        int freq_cand1=0, cand1;
+        int freq_cand2=0, cand2;
+        
         for (int i=0; i<n; i++) {
-            if (nums[i]==cand1) count1++;
-            else if (nums[i]==cand2) count2++;
-            else if (count1==0 && nums[i]!=cand2) {
-                count1=1;
+            if (nums[i]==cand1) freq_cand1++;
+            else if (nums[i]==cand2) freq_cand2++;
+            else if (freq_cand1==0 && nums[i]!=cand2) {
                 cand1= nums[i];
-            } else if (count2==0 && nums[i]!=cand1) {
+                freq_cand1=1;
+            } 
+            else if (freq_cand2==0 && nums[i]!=cand1) {
                 cand2= nums[i];
-                count2=1;
+                freq_cand2=1;
             } else {
-                count1--;
-                count2--;
+                freq_cand1--;
+                freq_cand2--;               
             }
-        }        
-
-        vector<int> res;
-        int countcand1=0, countcand2=0;
-        for (int i=0; i<n; i++) {
-            if (nums[i]==cand1) countcand1++;
-            else if (nums[i]==cand2) countcand2++;
         }
 
-        if (countcand1> (n/3)) res.push_back(cand1);
-        if (countcand2> (n/3)) res.push_back(cand2);
-        return res;
+        int count_cand1=0, count_cand2=0;
+        for (int i=0; i<n; i++) {
+            if (nums[i]==cand1) count_cand1++;
+            else if (nums[i]==cand2) count_cand2++;
+        }
+
+        vector<int> res;
+        if (count_cand1> (n/3)) res.push_back(cand1);
+        if (count_cand2> (n/3)) res.push_back(cand2);
+        return res;        
     }
 };
