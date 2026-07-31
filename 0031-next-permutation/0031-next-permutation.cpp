9512046c686756b636e75552b4f9d8dd3bb5e39c
Dakshit Mohan <dakshitmohan@gmail.com>
2026-07-31 19:33:06 +0530

    Time: 1 ms (3.79%), Space: 15.8 MB (45.92%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n= nums.size();
-        int pivot= -1;
-
+        if (n==0 || n==1) return;
+        int pivot=-1;
+        //finding pivot
         for (int i=n-2; i>=0; i--) {
-            if(nums[i]<nums[i+1]) {
+            if (nums[i]<nums[i+1]) {
                 pivot= i;
                 break;
             }
         }
-
-        if (pivot == -1) {
+        if (pivot==-1) {
             reverse(nums.begin(), nums.end());
             return;
-        } // edge case
+        }
 
-        int j = n - 1;
-        while (nums[j] <= nums[pivot]) {
-            j--;    
-        }     
+        //finding next immediate
+        for (int i=n-1; i>=pivot+1; i--) {
+            if (nums[pivot]<nums[i]) {
+                swap(nums[pivot], nums[i]);
+                break;
+            }
+        }
 
-        swap(nums[j], nums[pivot]);
-
+        //reverse the remaining part
         reverse(nums.begin()+pivot+1, nums.end());
-
         return;
     }
 };
