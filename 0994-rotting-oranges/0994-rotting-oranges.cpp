1208eddfdeee85e74ab1714c9d5fdf924b0be77c
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-03 21:57:59 +0530

    Time: 0 ms (100%), Space: 16.8 MB (70.09%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int rows= grid.size();
-        int colms= grid[0].size();
-        int arr[rows][colms];
-        queue<pair<pair<int, int>, int>> q; // row, colm, time
+        int row= grid.size();
+        int col= grid[0].size();
+        int vis[row][col];
+        queue<pair<pair<int, int>, int>> q; // row, col, time
 
-        //initializing grid
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<colms; j++) {
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
                 if (grid[i][j]==2) {
+                    vis[i][j]=2;
                     q.push({{i, j}, 0});
-                    arr[i][j]=2;
-                } else arr[i][j]= 0;
+                } else vis[i][j]=0;
             }
         }
 
-        int minutes=0;
-        int dr[]= {0,0,+1,-1};
-        int dc[]= {-1,+1,0, 0};
+        int res=0;
+        //neighbour grid element i,j
+        int drow[]= {+1,-1,0,0};
+        int dcol[]= {0, 0, -1, +1};
         while (!q.empty()) {
             auto curr= q.front();
             q.pop();
             int cr= curr.first.first;
             int cc= curr.first.second;
             int ct= curr.second;
-            minutes= max(minutes, ct);
-            //check all neighbours
+            res= max(res, ct);
+
             for (int i=0; i<4; i++) {
-                int r= cr+ dr[i];
-                int c= cc+ dc[i];
-                if (r<rows && c<colms && r>=0 && c>=0 && grid[r][c]==1 && arr[r][c]!=2){
-                    arr[r][c]=2;
-                    q.push({{r, c}, ct+1});
-                }
+                int nr= cr+ drow[i];
+                int nc= cc+ dcol[i];
+                if (nr>=0 && nc>=0 && nr<row && nc<col && grid[nr][nc]==1 && vis[nr][nc]==0) {
+                    vis[nr][nc]= 2;
+                    q.push({{nr, nc}, ct+1});
+                } 
             }
         }
 
-        //check for -1 condition
-        for (int i=0; i<rows; i++) {
-            for (int j=0; j<colms; j++) {
-                if (arr[i][j]!=2 && grid[i][j]==1) return -1;
+        for (int i=0; i<row; i++) {
+            for (int j=0; j<col; j++) {
+                if (grid[i][j]==1 && vis[i][j]==0) return -1;
             }
         }
 
-        return minutes;
+        return res;
     }
 };
