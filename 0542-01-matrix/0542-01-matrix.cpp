c492bdd87bcce5bf8dfec3445836b1a0c8c6695c
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-06 12:14:03 +0530

    Time: 16 ms (66.61%), Space: 38.9 MB (29.99%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int n= mat.size();
+        int m= mat[0].size();
+        queue<pair<pair<int, int>, int>> q; // row, col, dist
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>> grid(n, vector<int>(m, 0));
+        //filling vis 
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (mat[i][j]==0) {
+                    q.push({{i,j},0});
+                    vis[i][j]=1;
+                } 
+            }
+        }
+        
+        vector<int> dr= {-1,1,0,0};
+        vector<int> dc= {0,0,-1,1};
+        while (!q.empty()) {
+            int cr= q.front().first.first;
+            int cc= q.front().first.second;
+            int dist= q.front().second;
+            q.pop();
+            grid[cr][cc]= dist;
+
+            for (int k=0; k<4; k++) {
+                int nr= cr+ dr[k];
+                int nc= cc+ dc[k];
+                if (nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0) {
+                    vis[nr][nc]= 1;
+                    q.push({{nr, nc}, dist+1});
+                }
+            }
+        }
+
+        return grid;
+    }
+};
