bd3599d9f418364ccdd0d62fcc5a20dda9ce2e3c
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-13 17:36:50 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.28%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    void permut_helper(vector<vector<int>> &res, vector<int> curr, int n, vector<int> nums) {
-        if (curr.size()==n && nums.size()==0) {
+    void permut_helper(vector<vector<int>> &res, vector<int> curr, vector<int> nums) {
+        if (nums.size()==0) {
             res.push_back(curr);
             return;
-        } 
-        for (int i = 0; i < nums.size(); i++) {
-            
-            // choose
+        }
+        for (int i=0; i<nums.size(); i++) {
             curr.push_back(nums[i]);
+            vector<int> remain= nums;
+            remain.erase(remain.begin()+ i);
 
-            // remove chosen element
-            vector<int> remaining = nums;
-            remaining.erase(remaining.begin() + i);
-
-            // recurse
-            permut_helper(res, curr, n, remaining);
-
-            // undo choice
+            permut_helper(res, curr, remain);
+            //backtrack
             curr.pop_back();
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> curr;
-        int n= nums.size();
+        int n=nums.size();
 
-        permut_helper(res, curr, n, nums);
-        
+        permut_helper(res, curr, nums);
         return res;
     }
 };
