0224f3c3e0b640c0e8e5434d8ef290287f0f5874
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-13 10:17:54 +0530

    Time: 274 ms (9.36%), Space: 24 MB (7.7%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,39 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    int height(TreeNode* node) {
+        if (node == nullptr) return 0;
+
+        int leftht= 1+ height(node->left);
+        int rightht= 1+ height(node->right);
+
+        return max(leftht, rightht);
+    }
+
     int diameterOfBinaryTree(TreeNode* root) {
-        stack<TreeNode*> s;
-        unordered_map<TreeNode*, int /* diameter*/> m;
-        if (root) s.push(root);
-        int res=0;
-        
-        while(!s.empty()){
-            TreeNode* curr= s.top();
-            if (curr->left && m.find(curr->left)==m.end()){
-                s.push(curr->left);
-            } else if (curr->right && m.find(curr->right)==m.end()){
-                s.push(curr->right);
-            } else {
-                s.pop();
-                int leftHeight = curr->left ? m[curr->left] : 0;
-                int rightHeight = curr->right ? m[curr->right] : 0;
-                // Store height of current node
-                m[curr] = 1 + max(leftHeight, rightHeight);
-                // Update diameter
-                res = max(res, leftHeight + rightHeight);
-            }
-        }
-        
-        return res;
+        if (root == nullptr) return 0;
+
+        int currentDia= height(root->left) + height(root->right);
+
+        int leftDia= diameterOfBinaryTree(root->left);
+        int rightDia= diameterOfBinaryTree(root->right);
+
+        return max(currentDia, max(leftDia, rightDia));
     }
 };
