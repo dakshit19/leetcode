ac3dd4c5430518f39851f15cae6203fd70d09077
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-15 14:31:09 +0530

    Time: 16 ms (52.43%), Space: 29.3 MB (47.21%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.cpp
@@ -5,18 +5,18 @@ public:
             res.push_back(curr);
             return;
         }
-
         //take 1
         helper(n, res, curr+'1');
         //check if valid then insert 0
-        if (curr.empty()||curr.back()=='1')
+        if (curr.empty()||curr.back()=='1'){
             helper(n, res, curr+'0');
+        }
     }
     vector<string> validStrings(int n) {
         vector<string> res;
         string curr="";
 
-        helper(n, res, curr );
+        helper(n, res, curr);
         return res;
     }
 };
