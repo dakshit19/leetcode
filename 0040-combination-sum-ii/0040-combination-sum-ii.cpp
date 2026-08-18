4c3ac4be3966aa3b63554b5a294fc99fc26f7960
Dakshit Mohan <dakshitmohan@gmail.com>
2026-08-18 12:26:36 +0530

    Time: 106 ms (5.37%), Space: 41.6 MB (5.17%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-    void helper(vector<vector<int>> &res, vector<int> subs,
-                const vector<int> &nums, int targ, int idx, int sum) {
-
+    void helper(vector<vector<int>> &res, vector<int> subs, const vector<int> &nums, int targ, int idx, int sum) {
         if (sum > targ) return;
-
         if (sum == targ) {
             res.push_back(subs);
             return;
         }
-
         if (idx == nums.size()) return;
 
-        // TAKE
         subs.push_back(nums[idx]);
-        helper(res, subs, nums, targ, idx + 1, sum + nums[idx]);
+        helper(res, subs, nums, targ, idx + 1, sum + nums[idx]);//take
 
-        // NOT TAKE
-        subs.pop_back();
-
+        subs.pop_back(); //not take
         idx++;
-
         // skip duplicate values
         while (idx < nums.size() && nums[idx] == nums[idx - 1]) {
             idx++;
         }
-
         helper(res, subs, nums, targ, idx, sum);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
-
         vector<vector<int>> res;
         vector<int> curr;
-
         helper(res, curr, candidates, target, 0, 0);
-
         return res;
     }
 };
